4b7b953102bfe469940470c6c56c1f0020f19366
Breno Baptista <47641641+brenobaptista@users.noreply.github.com>
2026-04-22 15:24:14 -0300

    Add brokerId and companySearch filters

--- backend/submissions/filters/submission.py
+++ backend/submissions/filters/submission.py
@@ -11,8 +11,10 @@ class SubmissionFilterSet(django_filters.FilterSet):
     """
 
     status = django_filters.CharFilter(field_name="status", lookup_expr="iexact")
+    brokerId = django_filters.NumberFilter(field_name="broker")
+    companySearch = django_filters.CharFilter(field_name="company__legal_name", lookup_expr="icontains")
 
     class Meta:
         model = models.Submission
-        fields = ["status"]
+        fields = ["status", "brokerId", "companySearch"]
 
